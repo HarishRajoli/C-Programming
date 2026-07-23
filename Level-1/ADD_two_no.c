139fc7949f5ba357d55f861d29867ecc6c1ed512
HarishRajoli <hvrajoli22@gmail.com>
2026-07-23 20:43:23 +0530

    Add files via upload

---
+++ Level-1/ADD_two_no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)
+{
+    int a,b,add;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    add=a+b;
+    printf("the addition of two number is :%d",add);
+    return 0;
+}
